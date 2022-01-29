bb34475743ec374896206425d37750f0cc0d2360
flychensc <flychensc@163.com>
2022-01-30 01:54:27 +0800

    refine: check before slice

--- magic.py
+++ magic.py
@@ -9,34 +9,33 @@ import talib
 
 
 def classify(context, order_book_id, day, historys):
-    if day in context.cache and order_book_id in context.cache[day]:
-        order_day = context.cache[day].pop(order_book_id)
-        if not len(context.cache[day]):
-            context.cache.pop(day)
+    order_day = context.cache[day].pop(order_book_id)
+    if not len(context.cache[day]):
+        context.cache.pop(day)
 
-        label = "holding"
-        if historys['datetime'][0] == np.int64(order_day.strftime("%Y%m%d%H%M%S")):
-            # 数据正常
-            price = historys['close'][0]
-            stop_loss = price*context.STOP_LOSS
-            take_profit = price*context.TAKE_PROFIT
-            for price in historys['close']:
-                if price < stop_loss:
-                    label = "loss"
-                    break
-                if price > take_profit:
-                    label = "profit"
-                    break
+    label = "holding"
+    if historys['datetime'][0] == np.int64(order_day.strftime("%Y%m%d%H%M%S")):
+        # 数据正常
+        price = historys['close'][0]
+        stop_loss = price*context.STOP_LOSS
+        take_profit = price*context.TAKE_PROFIT
+        for price in historys['close']:
+            if price < stop_loss:
+                label = "loss"
+                break
+            if price > take_profit:
+                label = "profit"
+                break
 
-            context.picking.loc[(context.picking['date'] == order_day) & (context.picking['order_book_id'] == order_book_id), 'classify'] = label
-        else:
-            # 定位入选日，只会找到一项[0]，且只需要这项的行号[0]
-            delta_days = np.where(historys['datetime'] == np.int64(order_day.strftime("%Y%m%d%H%M%S")))[0][0]
-            # 继续平移停牌的这段时间
-            verify_day = day + datetime.timedelta(days=int(delta_days))
-            context.cache.setdefault(verify_day, {})
-            # 价格可能复权，所以保存入选日期
-            context.cache[verify_day][order_book_id] = order_day
+        context.picking.loc[(context.picking['date'] == order_day) & (context.picking['order_book_id'] == order_book_id), 'classify'] = label
+    else:
+        # 定位入选日，只会找到一项[0]，且只需要这项的行号[0]
+        delta_days = np.where(historys['datetime'] == np.int64(order_day.strftime("%Y%m%d%H%M%S")))[0][0]
+        # 继续平移停牌的这段时间
+        verify_day = day + datetime.timedelta(days=int(delta_days))
+        context.cache.setdefault(verify_day, {})
+        # 价格可能复权，所以保存入选日期
+        context.cache[verify_day][order_book_id] = order_day
 
 
 def init(context):
@@ -66,8 +65,9 @@ def after_trading(context):
         # 免费的日级别数据每个月月初更新，下载命令: rqalpha download-bundle
         historys = history_bars(order_book_id, context.BAR_COUNT, context.FREQUENCY, fields=['datetime', 'close'], include_now=True)
 
-        # 包含入选日
-        classify(context, order_book_id, day, historys[-1-context.POSITION_DAY:])
+        if day in context.cache and order_book_id in context.cache[day]:
+            # 包含入选日
+            classify(context, order_book_id, day, historys[-1-context.POSITION_DAY:])
 
         # 今日无数据: 停牌
         if historys['datetime'][-1] < np.int64(day.strftime("%Y%m%d%H%M%S")):
